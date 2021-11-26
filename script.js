1d5adb250f1a3d2b7e2daeaf94da667cc199db6b
yammybajwa <92578770+yammybajwa@users.noreply.github.com>
2021-11-26 07:28:36 -0800

    get button by js

---
+++ script.js
@@ -0,0 +1,4 @@
+let myCount = 0;
+const result = document.getElementById("result");
+const positiveBtn = document.getElementById("positiveBtn");
+const negativeBtn = document.getElementById("negativeBtn");
