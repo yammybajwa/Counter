4fa3540434dcfe8de1a3323ab83b1b100dbd3af1
yammybajwa <92578770+yammybajwa@users.noreply.github.com>
2021-11-26 07:29:07 -0800

    working on plus btn

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let myCount = 0;
 const result = document.getElementById("result");
 const positiveBtn = document.getElementById("positiveBtn");
 const negativeBtn = document.getElementById("negativeBtn");
+positiveBtn.onclick = function() {
+    //increment method in counter
+    myCount += 1;
+    result.innerText = myCount;
+};
